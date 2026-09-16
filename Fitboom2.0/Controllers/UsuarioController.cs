d0243baed3a71731c9f3ee8987853c238049dd17
Santiago Rivera <edgarsantiagoriverabecerra@gmail.com>
2026-09-16 08:33:53 -0500

    prueba

---
+++ Fitboom2.0/Controllers/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FITBOOM.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("La vista Usuario funciona");
+        }
+    }
+}
---
+++ Fitboom2.0/Models/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TuProyecto.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int IdUsuario { get; set; }
+
+    public string Nombre { get; set; }
+
+        public string Correo { get; set; }
+
+        public string Contrasena { get; set; }
+
+        public string TipoUsuario { get; set; }
+    }
+
+}
