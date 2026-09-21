137cd5db52df8b39ddcb5216609139b61c328416
Santiago Rivera <edgarsantiagoriverabecerra@gmail.com>
2026-09-21 11:27:00 -0500

    cambios 1

---
+++ Fitboom2.0/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Fitboom2._0.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "El correo es obligatorio.")]
+        [EmailAddress(ErrorMessage = "Ingresa un correo electrónico válido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "La contraseña es obligatoria.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "¿Recordarme?")]
+        public bool RememberMe { get; set; }
+    }
+}
